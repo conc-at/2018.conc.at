9807854541590a0b59d9b75bf83a83a3d9a7e5a4
Hannes Moser <hannes@impossiblearts.com>
2017-04-16 16:08:07 +0200

    feat: basic layout

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,2 @@
 import '../styles/main.scss'
+import 'font-awesome-webpack2'
--- config/base.js
+++ config/base.js
@@ -65,11 +65,16 @@ module.exports = function() {
             fallback: 'style-loader'
           })
         },
+        // Font Awesome
+        {
+          test: /\.(ttf|otf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
+          loader: 'file-loader?name=fonts/[name].[ext]'
+        },
         // Images
         // Inline base64 URLs for <=8k images, direct URLs for the rest
         {
           test: /\.(png|jpg|jpeg|gif|svg)$/,
-          loader: 'url',
+          loader: 'url-loader',
           query: {
             limit: 8192,
             name: 'images/[name].[ext]?[hash]'
@@ -77,7 +82,6 @@ module.exports = function() {
         }
       ]
     },
-
     plugins
   }
 }
